9b49d4c209dcbd42508d51ca603e5f6c15372cc7
duypv1 <v.duypv1@vinai.io>
2022-02-15 16:12:52 +0000

    object detection

---
+++ README.md
@@ -0,0 +1,97 @@
+# Unbiased Teacher for Semi-Supervised Object Detection
+
+<img src="teaser/pytorch-logo-dark.png" width="10%"> [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+**Unbiased Teacher for Semi-Supervised Object Detection**<br>
+[Yen-Cheng Liu](https://ycliu93.github.io/), [Chih-Yao Ma](https://chihyaoma.github.io/), [Zijian He](https://research.fb.com/people/he-zijian/), [Chia-Wen Kuo](https://sites.google.com/view/chiawen-kuo/home), [Kan Chen](https://kanchen.info/), [Peizhao Zhang](https://scholar.google.com/citations?user=eqQQkM4AAAAJ&hl=en), [Bichen Wu](https://scholar.google.com/citations?user=K3QJPdMAAAAJ&hl=en), [Zsolt Kira](https://www.cc.gatech.edu/~zk15/), [Peter Vajda](https://sites.google.com/site/vajdap)<br>
+International Conference on Learning Representations (ICLR), 2021 <br>
+
+[[arXiv](https://arxiv.org/abs/2102.09480)] [[OpenReview](https://openreview.net/forum?id=MJIve1zgR_)] [[Project](https://ycliu93.github.io/projects/unbiasedteacher.html)]
+
+<p align="center">
+<img src="teaser/figure_teaser.gif" width="85%">
+</p>
+
+# Installation
+
+## Prerequisites
+
+- Linux or macOS with Python ≥ 3.6
+- PyTorch ≥ 1.5 and torchvision that matches the PyTorch installation.
+
+## Install PyTorch in Conda env
+
+```shell
+# create conda env
+conda create -n detectron2 python=3.6
+# activate the enviorment
+conda activate detectron2
+# install PyTorch >=1.5 with GPU
+conda install pytorch torchvision -c pytorch
+```
+
+## Build Detectron2 from Source
+
+Follow the [INSTALL.md](https://github.com/facebookresearch/detectron2/blob/master/INSTALL.md) to install Detectron2.
+
+## Fine-tuning
+
+- Train the Unbiased Teacher under 10% wheat-detection-supervision (with COCO format)
+
+```shell
+python train_net.py \
+      --num-gpus 1 \
+      --config configs/coco_supervision/cus_config.yaml \
+      DATASETS.NAME wheat-detection \
+      DATASETS.TRAIN_IMAGE_DIR datasets/wheat-detection/train2017 \
+      DATASETS.TRAIN_ANN_PATH datasets/wheat-detection/annotations/instances_train2017.json \
+      OUTPUT_DIR output_exdark/ \
+      DATALOADER.SUP_PERCENT 30.0 \
+      SOLVER.MAX_ITER 10000 \
+      MODEL.ROI_HEADS.NUM_CLASSES 1
+```
+- After fine-tune on 300 images (10% of 3373 images) of the [Global Wheat Detection](https://www.kaggle.com/c/global-wheat-detection/data) dataset.
+
+<p align="center">
+<img src="teaser/tmp.jpg" width="85%">
+</p>
+
+- After fine-tune on 680 images (10% of 6823 images) of the Exdark dataset.
+
+<p align="center">
+<img src="teaser/tmp_exdark.jpg" width="85%">
+</p>
+
+- Use the inference.py file to predict the bounding box after fine-tune the model.
+
+
+```BibTeX
+@inproceedings{liu2021unbiased,
+    title={Unbiased Teacher for Semi-Supervised Object Detection},
+    author={Liu, Yen-Cheng and Ma, Chih-Yao and He, Zijian and Kuo, Chia-Wen and Chen, Kan and Zhang, Peizhao and Wu, Bichen and Kira, Zsolt and Vajda, Peter},
+    booktitle={Proceedings of the International Conference on Learning Representations (ICLR)},
+    year={2021},
+}
+```
+
+```BibTeX
+@misc{wu2019detectron2,
+  author =       {Yuxin Wu and Alexander Kirillov and Francisco Massa and
+                  Wan-Yen Lo and Ross Girshick},
+  title =        {Detectron2},
+  howpublished = {\url{https://github.com/facebookresearch/detectron2}},
+  year =         {2019}
+}
+```
+
+
+
+```shell
+
+python train_net.py --num-gpus 1 --config configs/coco_supervision/cus_config.yaml DATASETS.NAME dota DATASETS.TRAIN_IMAGE_DIR datasets/dota/train/images/ DATASETS.TRAIN_ANN_PATH datasets/dota/train/dota_train_coco.json MODEL.ROI_HEADS.NUM_CLASSES 16 OUTPUT_DIR output_dota/ DATALOADER.SUP_PERCENT 50.0 SOLVER.MAX_ITER 10000 \
+&& python inference.py --test_dir datasets/dota/test/images/ --output_dir visualize_test_dota/ --model_dir output_dota/ \
+&& python train_net.py --num-gpus 1 --config configs/coco_supervision/cus_config.yaml DATASETS.NAME exdark DATASETS.TRAIN_IMAGE_DIR datasets/exdark_data/exdark/ DATASETS.TRAIN_ANN_PATH datasets/exdark_data/exdark_anno_train.json MODEL.ROI_HEADS.NUM_CLASSES 12 OUTPUT_DIR output_exdark/ DATALOADER.SUP_PERCENT 30.0 SOLVER.MAX_ITER 10000 \
+&& python inference.py --test_dir datasets/exdark_data/test/ --output_dir visualize_test_exdark/ --model_dir output_exdark/ \
+&& python train_net.py --num-gpus 1 --config configs/coco_supervision/cus_config.yaml DATASETS.NAME wheat-detection DATASETS.TRAIN_IMAGE_DIR datasets/wheat-detection/train2017 DATASETS.TRAIN_ANN_PATH datasets/wheat-detection/annotations/instances_train2017.json MODEL.ROI_HEADS.NUM_CLASSES 1 OUTPUT_DIR output_wheat/ DATALOADER.SUP_PERCENT 10.0 SOLVER.MAX_ITER 10000 \
+&& python inference.py --test_dir datasets/wheat-detection/test/ --output_dir visualize_test_wheat/ --model_dir output_wheat/
+```
---
+++ inference.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.engine import default_argument_parser, default_setup, launch
+
+from ubteacher import add_ubteacher_config
+from ubteacher.engine.trainer import UBTeacherTrainer, BaselineTrainer
+
+# hacky way to register
+from ubteacher.modeling.meta_arch.rcnn import TwoStagePseudoLabGeneralizedRCNN
+from ubteacher.modeling.proposal_generator.rpn import PseudoLabRPN
+from ubteacher.modeling.roi_heads.roi_heads import StandardROIHeadsPseudoLab
+import ubteacher.data.datasets.builtin
+
+from ubteacher.modeling.meta_arch.ts_ensemble import EnsembleTSModel
+
+import cv2
+import PIL
+import os
+import torch
+import numpy as np
+from detectron2.utils.visualizer import Visualizer
+from detectron2.utils.visualizer import ColorMode
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets import register_coco_instances, load_coco_json
+
+
+def setup(model_dir):
+    cfg = get_cfg()
+    add_ubteacher_config(cfg)
+    cfg.merge_from_file(os.path.join(model_dir, "config.yaml"))
+    cfg.MODEL.WEIGHTS = os.path.join(model_dir, "model_final.pth")
+    
+    # register_dataset
+    cfg.DATASETS.TRAIN = (cfg.DATASETS.NAME+"_train", )
+    register_coco_instances(cfg.DATASETS.TRAIN[0], {}, cfg.DATASETS.TRAIN_ANN_PATH, cfg.DATASETS.TRAIN_IMAGE_DIR)
+    # cfg.DATASETS.TEST = (cfg.DATASETS.NAME+"_val", )
+    # register_coco_instances(cfg.DATASETS.TEST[0], {}, cfg.DATASETS.TEST_ANN_PATH, cfg.DATASETS.TEST_IMAGE_DIR)
+
+    load_coco_json(cfg.DATASETS.TRAIN_ANN_PATH, cfg.DATASETS.TRAIN_IMAGE_DIR, cfg.DATASETS.TRAIN[0])
+
+    return cfg
+
+
+def build_model(cfg):
+    Trainer = UBTeacherTrainer
+    model = Trainer.build_model(cfg)
+    model_teacher = Trainer.build_model(cfg)
+    ensem_ts_model = EnsembleTSModel(model_teacher, model)
+    DetectionCheckpointer(ensem_ts_model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.WEIGHTS, resume="resume")
+    ensem_ts_model.load_state_dict(torch.load(cfg.MODEL.WEIGHTS)["model"])
+
+    model = ensem_ts_model.modelTeacher
+    model.eval()
+    return model
+
+
+def inference(cfg, model, image_path, threshold=0.7, visualize=None): #visualize is path output of image visualize
+    # image = cv2.imread(image_path)
+    pil_image = PIL.Image.open(image_path).convert('RGB') 
+    image = np.array(pil_image)
+    image = image[:, :, ::-1].copy()
+
+    inputs = torch.from_numpy(np.transpose(image, (2, 0, 1)))
+    outputs = model([{"image":inputs}])
+    instances = outputs[0]["instances"].to("cpu")
+    instances = instances[instances.scores > threshold]
+    
+    if visualize is not None:
+        v = Visualizer(image[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]))
+        out = v.draw_instance_predictions(instances)
+        cv2.imwrite(visualize, out.get_image()[:, :, ::-1])
+
+    return instances
+
+    """
+    Manual way
+
+    height, width = image.shape[:2]
+    image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+    inputs = [{"image": image, "height": height, "width": width}]
+    with torch.no_grad():
+        images = model.preprocess_image(inputs)  # don't forget to preprocess
+        features = model.backbone(images.tensor)  # set of cnn features
+        proposals, _ = model.proposal_generator(images, features, None)  # RPN
+
+        features_ = [features[f] for f in model.roi_heads.box_in_features]
+        box_features = model.roi_heads.box_pooler(features_, [x.proposal_boxes for x in proposals])
+        box_features = model.roi_heads.box_head(box_features)  # features of all 1k candidates
+        predictions = model.roi_heads.box_predictor(box_features)
+        pred_instances, pred_inds = model.roi_heads.box_predictor.inference(predictions, proposals)
+        pred_instances = model.roi_heads.forward_with_given_boxes(features, pred_instances)
+
+        # output boxes, masks, scores, etc
+        pred_instances = model._postprocess(pred_instances, inputs, images.image_sizes)  # scale box to orig size
+        # features of the proposed boxes
+        feats = box_features[pred_inds]
+    """
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_dir", default="visulize_test/", type=str,required=True, help="Directory output visualize test",)
+    parser.add_argument("--test_dir", default="datasets/dota/test/images/", type=str,required=True, help="Directory test images",)
+    parser.add_argument("--model_dir", default="output/", type=str,required=True, help="Directory model",)
+    parser.add_argument("--threshold", default=0.8, type=float,required=False, help="Threshold",)
+    args = parser.parse_args()
+
+    cfg = setup(args.model_dir)
+    model = build_model(cfg)
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    files = [args.test_dir + f for f in os.listdir(args.test_dir) if f.split(".")[-1].lower() in {"jpg", "png", "jpeg"}]
+    import random
+    from tqdm import tqdm
+    # files = random.choices(files, k=30)
+    for idx in tqdm(range(len(files))):
+        # idx = int(input("Input index image: "))
+        # if idx == -1:
+        #     break
+        # print(f"image_path: {files[idx]}")
+        output_file = args.output_dir + ".".join(files[idx].replace(args.test_dir, "").split(".")[:-1]) + ".jpg"
+        result = inference(cfg, model=model, image_path=files[idx], threshold=args.threshold, visualize=output_file)
+        # print(result)
+
+if __name__ == "__main__":
+    main()
